2798452c2fd06781322112ec48512e364f701a1a
IIPeteII <pera17ab@student.cbs.dk>
2023-02-20 17:46:53 +0100

    done

--- AFK script/AFK-script.py
+++ AFK script/AFK-script.py
@@ -1,11 +1,20 @@
 #imports
 
 import pyautogui
+import random
 import time
+import numpy as np
 
+#random variables
+
+x = np.random.randint(100,500)
+y = np.random.randint(100,500)
+z = np.random.randint(100,500)
+b = np.random.randint(100,500)
+num_seconds = 3
 #script
 
 while True:
-    pyautogui.moveRel(0,8)
+    pyautogui.moveTo(x,y)
     time.sleep(5)
-
+    pyautogui.dragTo(z,b,duration=num_seconds)
